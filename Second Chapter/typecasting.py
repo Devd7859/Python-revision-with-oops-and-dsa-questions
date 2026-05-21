9807307bf7c7470c74d0ef9590f4977ab33914ad
Devd7859 <Devdyadav4658@gmail.com>
2026-05-21 21:15:18 +0530

    Understanding type,typecastingg,input functions

---
+++ Second Chapter/typecasting.py
@@ -0,0 +1,17 @@
+a = 31
+t = type(a)
+print(t)
+b = "harry"
+t2 = type(b)
+c = 31.2
+t3 = type(c)
+print(t2)
+print(t3)
+d = "31.2"
+print(type(d))
+e = float(d)
+
+print(type(e))
+aa = int(input("Enter number 1: "))
+bb = int(input("Enter number 2: "))
+print ("The sum of two numbers is :", aa + bb)
