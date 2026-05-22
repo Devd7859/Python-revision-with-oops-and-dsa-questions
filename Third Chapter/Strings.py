487d01430feeed18c353b2f8732a29e1a08b8e9b
Devd7859 <Devdyadav4658@gmail.com>
2026-05-22 16:52:27 +0530

    understanding strings in python

---
+++ Third Chapter/Strings.py
@@ -0,0 +1,23 @@
+#three ways to define a string
+name = 'harry'
+a = "dev"
+b = '''chetanya'''
+#string slicing
+name2 = name[0:3] #start from index 0 to all the way till 3 excluding 3
+
+#print(name2)
+character = name[1]
+#print(character)
+#slicing with skip values
+word = "amazing"
+word[1:6:2]
+#string functionss
+print(len(name))
+print(name.endswith("rry"))
+print(name.startswith("ha"))
+print(name.capitalize())
+#string escape sequence
+n = "Dev is a good buy \n not a \"bad\" boy"
+#\t to give tab space
+print(n)
+
