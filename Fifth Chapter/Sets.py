be982aea1d6281dd01691564797f027cc6220e2f
Devd7859 <Devdyadav4658@gmail.com>
2026-05-23 19:36:22 +0530

    Understandings sets in python

---
+++ Fifth Chapter/Sets.py
@@ -0,0 +1,8 @@
+e = set() #creates an empty set
+#set doesnt contain repetitive items however you can have repetitive items but at the time of printing it wont show the repeted items
+#it doesnt maitain an order
+s = {1,5,54,5,5,5,32}
+print(s)
+#methods in sets
+s.add(566)
+print(s)
