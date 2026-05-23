8ea318464445e7386b1621081b2639c7ecb57186
Devd7859 <Devdyadav4658@gmail.com>
2026-05-23 19:14:00 +0530

    Understanding dictonary in python

---
+++ Fifth Chapter/Dictonary.py
@@ -0,0 +1,15 @@
+#Dictonary is a collection of key-value pairs in python
+#it is muttable
+#it is unordered
+#it is indexed
+#can not contain duplicate key
+#you can call value with the key
+#you can make list of list too but that is computaionally more expensive so you dont take it, basically works like a hashtable
+marks = {
+    "Harry" : 87,
+    "Dev" : 96,
+    "Rohan" : 80,
+}
+print(marks,type(marks))
+#Properties of 
+d = {} #empty dictonary
