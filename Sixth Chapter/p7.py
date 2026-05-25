82afb0c3481c3644caa391bf864f340d0a4874e0
Devd7859 <Devdyadav4658@gmail.com>
2026-05-25 20:57:49 +0530

    To check whether string is there in a particular list or not

---
+++ Sixth Chapter/p7.py
@@ -0,0 +1,5 @@
+post = "Hey harry bhai is good Harry is very good and harry is great"
+if "harry".lower() in post.lower():
+    print("this post is talking about harry.")
+else:
+    print("this post is not talking about harry")
